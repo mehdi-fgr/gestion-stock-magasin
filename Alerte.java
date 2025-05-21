6ac8d8fa88393c5e7e9beefed32f1e2bf18d5dc7
CyFlamant <cyrille.flamant@gmail.com>
2025-05-21 18:49:07 +0200

    Add files via upload

--- Alerte.java
+++ Alerte.java
@@ -2,6 +2,8 @@ package applicationGestionStock;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -44,6 +46,18 @@ private void initUI() {
 	panel1.add(zoneProduitsBas, BorderLayout.CENTER);
 	add(panelOk, BorderLayout.SOUTH);
 	panelOk.add(boutonOk, BorderLayout.CENTER);
+	
+	//Comportement du bouton OK
+	boutonOk.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed (ActionEvent e) {
+		dispose();
+		}
+	});
+	}
 }
-}
+
+
+
+
 
