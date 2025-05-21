c9e4156553e9b6717b38b965e78688a433950f1b
CyFlamant <cyrille.flamant@gmail.com>
2025-05-21 17:54:57 +0200

    Add files via upload
    Un premier exemple de fenêtre + une occasion de tester le github

---
+++ Alerte.java
@@ -0,0 +1,49 @@
+package applicationGestionStock;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class Alerte extends JFrame {
+
+//Attributs
+private JPanel panel1;
+private JLabel texteAlerte;
+private JTextArea zoneProduitsBas;
+private JButton boutonOk;
+private JPanel panelOk;
+
+//Constructeur
+public Alerte() {
+	super ("Alerte !");
+	setSize(400,400);
+	setLayout(new BorderLayout());
+	setLocationRelativeTo(null);
+	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	initUI();
+	setVisible(true);
+}
+//Méthodes
+private void initUI() {
+	//Initialisation du JPanel et des composants
+	panel1 = new JPanel(new BorderLayout());
+	texteAlerte = new JLabel("Les produits suivants ont un stock bas :");
+	texteAlerte.setPreferredSize(new Dimension(100,50));
+	zoneProduitsBas = new JTextArea("La liste des produits s'affiche ici");
+	boutonOk = new JButton("OK");
+	panelOk = new JPanel();
+	
+	//Ajout des JPanel dans la JFrame, puis ajout des composants dans les JPanel (le bouton OK est ajouté dans son propre JPanel pour ne pas occuper toute la zone "South")
+	add(panel1);
+	panel1.add(texteAlerte, BorderLayout.NORTH);
+	panel1.add(zoneProduitsBas, BorderLayout.CENTER);
+	add(panelOk, BorderLayout.SOUTH);
+	panelOk.add(boutonOk, BorderLayout.CENTER);
+}
+}
+
---
+++ MainGestionStock.java
@@ -0,0 +1,12 @@
+package applicationGestionStock;
+
+public class MainGestionStock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//Test fenetre Alerte
+new Alerte();
+	}
+
+}
